7e2b20f7480703533a1c781aab9652b35a567f7f
Al-Muhandis <mail@Renat.Su>
2018-07-01 19:52:11 +0400

    fix: Deleted unnecessary files

---
+++ fptelegram.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit fptelegram;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  tgsendertypes, tgtypes, longpoll_mthread, tgstatlog, tgutils;
+
+implementation
+
+end.
